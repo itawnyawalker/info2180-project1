2629c9cb8f77bbfd569c8467ba2ebb7325adde6e
Itawnya Walker <itawnya.walker@gmail.com>
2021-10-23 08:56:05 -0500

    Edited .html .css and .js files. New image file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
 /* Add your JavaScript to this file */
+
+window.onload = function() {
+	var form = document.querySelector('form');
+	var button = document.querySelector(".btn");
+	
+	
+	form.addEventListener('submit', function(element) {
+		element.preventDefault();
+		var email = document.querySelector('#email');
+		var msg = document.querySelector('.message');
+		
+		console.log("pass");
+	
+		if (email.value.trim().length != 0) {
+			msg.innerHTML = "Thank you! Your email address " + email.value + " has been added to our mailing list!";
+			email.value = "";
+		}
+		else{
+			msg.innerHTML = "Please enter a valid email address";
+			console.log("error");
+		}
+	})
+}
+
